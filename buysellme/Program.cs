ee1b897de4cafebd4cb79cb7ac8174ecd07cf618
Helen Allenet <helenallenet@Helens-MacBook-Air.local>
2021-10-23 16:42:16 +0100

    third commit

---
+++ buysellme/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace buysellme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to the buy sell me network, i am your server today, my name is Karen");
+
+            Thing one = new Thing("Gold Shoes");
+            Console.WriteLine(one.Name);
+
+            one.Name = "Platinum socks";
+            Console.WriteLine(one.Name);
+        }
+    }
+}
---
+++ buysellme/Thing.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace buysellme
+{
+    public class Thing
+    {
+        public Thing(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name { get; set; }
+    }
+}
